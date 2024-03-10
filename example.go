e61ade16331627f151f27c780bf7338101712a73
Atlas Ironic <atlasiro@un-l1.kzn.21-school.ru>
2024-03-10 21:50:34 +0300

    add_map

--- example.go
+++ example.go
@@ -1,31 +1,22 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"strings"
 
 	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 const (
 	BotToken   = "7145361114:AAGcDmLWHv9eyeQTjcj1djRA1oDcCJBmuKg"
-	WebhookURL = "https://87af-178-207-154-253.ngrok-free.app"
+	WebhookURL = "https://9fb9-178-207-154-253.ngrok-free.app"
 )
 
-var rss = map[string]string{
-	"Habr": "https://habrahabr.ru/rss/best/",
-}
-
-type RSS struct {
-	Items []Item `xml:"channel>item"`
-}
-
-type Item struct {
-	URL   string `xml:"guid"`
-	Title string `xml:"title"`
-}
+var (
+	messageMap = make(map[string]string)
+	imageMap   = make(map[string]string)
+)
 
 func initializeBot() (*tgbotapi.BotAPI, error) {
 	bot, err := tgbotapi.NewBotAPI(BotToken)
@@ -52,74 +43,36 @@ func startListening(bot *tgbotapi.BotAPI) {
 }
 
 func handleMessage(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	switch update.Message.Text {
-	case "/help":
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Available commands:\n"+
-			"/help - Show available commands\n"+
-			"Enter 'Habr' to get news from Habr")
+	command := strings.ToLower(update.Message.Text)
+	if response, ok := messageMap[command]; ok {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, response)
 		bot.Send(msg)
-	case "/add":
-		add := tgbotapi.NewMessage(update.Message.Chat.ID, "if you want add new file enter:\n"+"/add")
-		bot.Send(add)
-	default:
-		if url, ok := rss[update.Message.Text]; ok {
-			rss, err := getNews(url)
-			if err != nil {
-				bot.Send(tgbotapi.NewMessage(
-					update.Message.Chat.ID,
-					"sorry, error happened",
-				))
-			}
-			for _, item := range rss.Items {
-				bot.Send(tgbotapi.NewMessage(
-					update.Message.Chat.ID,
-					item.URL+"\n"+item.Title,
-				))
-			}
-		} else {
-			bot.Send(tgbotapi.NewMessage(
-				update.Message.Chat.ID,
-				"there is only Habr feed available",
-			))
-		}
+	} else if imageURL, ok := imageMap[command]; ok {
+		msg := tgbotapi.NewPhotoShare(update.Message.Chat.ID, imageURL)
+		bot.Send(msg)
+	} else {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Command not found")
+		bot.Send(msg)
 	}
 }
 
-func getNews(url string) (*RSS, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	rss := new(RSS)
-	err = xml.Unmarshal(body, rss)
-	if err != nil {
-		return nil, err
-	}
-
-	return rss, nil
-}
-
-func dop() {
+func start() {
 	bot, err := initializeBot()
 	if err != nil {
 		panic(err)
 	}
-
 	err = setWebhook(bot)
 	if err != nil {
 		panic(err)
 	}
-
 	startListening(bot)
 }
 
 func main() {
-	dop()
+	messageMap["/help"] = "Available commands:\n/help - Show available commands\n/add - Add a new command\n"
+	messageMap["/add"] = " ins  '/add hello:Hi there!'"
+	messageMap["/es"] = "Wtf"
+	imageMap["/cat"] = "https://s3.amazonaws.com/freecodecamp/running-cats.jpg"
+
+	start()
 }
